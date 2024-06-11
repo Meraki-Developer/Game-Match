3b86ffac15105948164bb6f0f8bbc1a91254a79e
Gerardo Martinez <gerardo.martinez@tectijuana.edu.mx>
2024-06-11 15:36:27 -0700

    Add animations pieces
    Agregando animacion a las piezas

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -31,6 +31,10 @@ public class Piece : MonoBehaviour
         x = x_;
         y = y_;
         board = board_;
+
+        transform.localScale = Vector3.one * 0.35f;
+        transform.DOScale(Vector3.one, 0.035f);
+
     }
 
     public void Move(int desX, int desY)
@@ -41,6 +45,28 @@ public class Piece : MonoBehaviour
             y = desY;
         };
     }
+
+    public void Remove(bool animated)
+    {
+        if (animated)
+        {
+            transform.DORotate(new Vector3(0, 0, -120f), 0.12f);
+            transform.DOScale(Vector3.one * 1.2f, 0.085f).onComplete = () =>
+            {
+                transform.DOScale(Vector3.zero, 0.1f).onComplete = () =>
+                {
+                    Destroy(gameObject);
+                };
+            };
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
 
     [ContextMenu("Test Move")]
     public void MoveTest()
